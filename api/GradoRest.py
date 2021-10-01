2a8f4272cffeafaaa14bf3bdc705cadce459a192
Pablo Daniel Villagrán <pdvillc@gmail.com>
2021-09-30 23:33:39 -0600

    agregado servicio de grado

---
+++ api/GradoRest.py
@@ -0,0 +1,74 @@
+from flasksetup import db, mellow, Resource, request, jsonify, api, app
+
+class Grado (db.Model):
+    id = db.Column('ID_GRADO', db.Integer, primary_key = True)
+    grado = db.Column('GRADO', db.String(100))
+  
+    def __init__(self, grado):
+        self.grado = grado
+
+class GradoSchema(mellow.Schema):
+    class Meta:
+        field = 'grado'
+
+grado = GradoSchema()
+grados = GradoSchema(many = True)
+
+class GradoManager(Resource):
+    pass
+
+    @staticmethod
+    def get():
+        try: id = request.args['id']
+        except Exception as _: id = None
+
+        if not id:
+            resultado = Grado.query.all()
+            return jsonify(grados.dump(resultado))
+        resultado = Grado.query.get(id)
+        return jsonify(grado.dump(resultado))
+
+    @staticmethod
+    def post():
+        grado = request.json['grado']
+
+        nuevo = Grado(grado)
+        db.session.add(nuevo)
+        db.session.commit()
+
+        return jsonify({
+            'resultado': f'Grado {grado} creado.'
+        })
+
+    @staticmethod
+    def put():
+        try: id = request.args['id']
+        except Exception as _: id = None
+        if not id:
+            return jsonify({
+                'resultado': f'Id {id} no encontrado.'
+            })
+        current = Grado.query.get(id)
+
+        current.grado = request.json['grado']
+
+        db.session.commit()
+        return jsonify({
+            'Message': f'Grado {current.grado} fue actualizado. '
+        })
+
+    @staticmethod
+    def delete():
+        try: id = request.args['id']
+        except Exception as _: id = None
+        if not id:
+            return jsonify({
+                'resultado': f'Id {id} no encontrado.'
+            })
+        current = Grado.query.get(id)
+        db.session.delete(current)
+        db.session.commit()
+        return jsonify({
+            'Message': f'Grado {current.grado} fue eliminado. '
+        })
+
--- api/flaskrun.py
+++ api/flaskrun.py
@@ -3,9 +3,11 @@ import CursoRest
 import SeccionRest
 import UsuarioRest
 import CarreraRest
+import GradoRest
 
 flask.api.add_resource(CarreraRest.CarreraManager, '/api/carrera')
 flask.api.add_resource(CursoRest.CursoManager, '/api/curso')
+flask.api.add_resource(GradoRest.GradoManager, '/api/grado')
 flask.api.add_resource(SeccionRest.SeccionManager, '/api/seccion')
 flask.api.add_resource(UsuarioRest.UsuarioManager, '/api/usuario')
 
