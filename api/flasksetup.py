63bc22c7327994b3f3a84637d44842e974cac342
Pablo Daniel Villagrán <pdvillc@gmail.com>
2021-10-01 19:26:21 -0600

    fix CORS?

--- api/flasksetup.py
+++ api/flasksetup.py
@@ -3,6 +3,7 @@ from flask.templating import render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_restful import Resource, Api
+from flask_cors import CORS
 import conexion as c_params
 
 app = Flask(__name__)
@@ -10,6 +11,9 @@ api = Api(app)
 con_string = "mariadb+mysqlconnector://"+c_params.con_string
 app.config['SQLALCHEMY_DATABASE_URI'] = con_string
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+CORS(app)
+
 db = SQLAlchemy(app)
 mellow = Marshmallow(app)
 
